ebc2cede3ba76540a84434ee653d0c4c6c891f11
Sascha Huber <dersaschahuber@web.de>
2019-06-01 22:28:05 +0200

    Manager refactored

--- blueprints/weather/WeatherManager.py
+++ blueprints/weather/WeatherManager.py
@@ -1,8 +1,13 @@
+from Utils.ServerData import ServerData
+
+
 class WeatherManager:
     def __init__(self):
         return
 
     def get_weather(self, location):
+        api_key = ServerData().get("OPEN_WEATHER_MAP_API_KEY")
+
         return {}
 
     def get_cities(self, keyword=None):
